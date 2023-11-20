1ee717ae606c06d6bf945717f2c209a301801504
JUKI <jyukipann@yahoo.co.jp>
2023-11-20 13:55:25 +0900

    Update D.py

--- hhkb/D.py
+++ hhkb/D.py
@@ -1,5 +1,17 @@
 import numpy as np
 N, M = list(map(int, input().split()))
-A = np.array(list(map(int, input().split())), dtype=int)-1
-B = np.array(list(map(int, input().split())), dtype=int)-1
-X = np.zeros((N, ), dtype=int)
+A = np.array(list(map(int, input().split())), dtype=int) - 1
+B = np.array(list(map(int, input().split())), dtype=int) - 1
+X = np.zeros((N, ), dtype=int) - 1
+graph = {i:list() for i in range(N)}
+for i in range(M):
+    graph[A[i]].append(B[i]) 
+    graph[B[i]].append(A[i])
+
+stack = graph[i]
+while len(stack) > 0:
+    node = stack.pop(0)
+    if X[i] == -1:
+        stack = graph[node] + stack
+    else:
+        
